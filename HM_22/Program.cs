8266ef248627ca4133f7604a3b62525453d81a8e
AkhunovAziz <akhunovaziz735@gmail.com>
2023-10-25 20:11:15 +0500

    Добавьте файлы проекта.

---
+++ HM_22/Program.cs
@@ -0,0 +1,18 @@
+﻿using HM_22;
+using System.Data.SqlClient;
+
+Console.WriteLine("\t\t1.Registratsiya\n\t\t2.Xabar Yuborish");
+int change = int.Parse(Console.ReadLine());
+var service = new Service();
+//service.CreateTable_Users();
+//service.CreateTable_SMS();
+switch (change)
+{
+    case 1:
+        service.AddUser();
+        break;
+    case 2:
+        service.AddSMS(); 
+        break;
+}
+
---
+++ HM_22/Service.cs
@@ -0,0 +1,117 @@
+﻿using System.Data.SqlClient;
+
+namespace HM_22
+{
+    public class Service
+    {
+        public void CreateTable_Users()
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=HM_22;Trusted_Connection=True;";
+
+                var query =
+                    @$"CREATE TABLE dbo.Users
+        (
+           ID int IDENTITY(1,1) NOT NULL,
+                    FirstName nvarchar(50) NULL,
+                    LastName nvarchar(50) NULL,
+                    Email nvarchar(50),
+                    CONSTRAINT pk_id PRIMARY KEY (ID)
+        );";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                try
+                {
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Table Created Succesfully");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error!!");
+                }
+            }
+        }
+        public void CreateTable_SMS()
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=HM_22;Trusted_Connection=True;";
+
+                var query =
+                    @$"CREATE TABLE dbo.SMS
+        (
+           ID int IDENTITY(1,1) NOT NULL,
+                    Sender nvarchar(50) NULL,
+                    Recipient nvarchar(50) NULL,
+                    Message nvarchar(250) NULL,
+        );";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                try
+                {
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Table Created Succesfully");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error!!");
+                }
+            }
+        }
+        public void AddUser()
+        {
+            Console.WriteLine("Ismingizni kiriting: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Familiyangizni kiriting: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Email kiriting: ");
+            string email = Console.ReadLine();
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=HM_22;Trusted_Connection=True;";
+                var querry = @$"
+                INSERT INTO dbo.Users
+                Values('{Name}','{lastName}','{email}');";
+
+                SqlCommand cmd = new SqlCommand(querry, connection);
+                try
+                {
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("User Created Succesfully");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+        }
+        public void AddSMS()
+        {
+            Console.Write("Id ingizni kiriting: ");
+            int sender = int.Parse(Console.ReadLine());
+            Console.Write("Habar yubormoqchi bulgan userni Id sini kiriting: ");
+            int recipient = int.Parse(Console.ReadLine());
+            Console.Write("Xabar yozing: ");
+            string message = Console.ReadLine();
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=HM_22;Trusted_Connection=True;";
+                var querry = @$"Insert into dbo.SMS VALUES({sender},{recipient},'{message}');";
+                SqlCommand cmd = new SqlCommand(querry, connection);
+                try
+                {
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Massage is sended!!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+        }
+    }
+}
